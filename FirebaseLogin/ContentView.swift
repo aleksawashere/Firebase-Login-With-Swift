ec0d164d47e2b22bfa24d15c83ad47271faf4f0c
aleksawashere <aleksa.dimitrijevic.business@gmail.com>
2022-06-25 12:39:24 +0200

    Initial Commit

---
+++ FirebaseLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseLogin
+//
+//  Created by Aleksa Dimitrijevic on 25.6.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseLogin/FirebaseLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseLoginApp.swift
+//  FirebaseLogin
+//
+//  Created by Aleksa Dimitrijevic on 25.6.22..
+//
+
+import SwiftUI
+
+@main
+struct FirebaseLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
